d5365d4f7d469316798552f4985328d23f31b4f5
David Aryasetia <david.aryasetia@gmail.com>
2024-01-07 15:16:57 +0700

    test repo

--- resources/views/jadwal.blade.php
+++ resources/views/jadwal.blade.php
@@ -3,11 +3,11 @@
 @section('container')
 
 <div class="pagetitle">
-    <h1>Atur Periode Pengisian</h1>
+    <h1>Atur Pengisian AMI</h1>
     <nav>
       <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-        <li class="breadcrumb-item active">Periode Pengisian</li>
+        <li class="breadcrumb-item active">Periode Pengisian AMI</li>
       </ol>
     </nav>
   </div>
