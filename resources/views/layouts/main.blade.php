98db89c241c775dd5d5d1a10757a35c5a506b5b6
davidaryasetia <david.aryasetia@gmail.com>
2024-11-21 20:19:53 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -440,7 +440,12 @@
             });
         });
     </script>
-    
+    <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/sidebarmenu.js') }}"></script>
+    <script src="{{ asset('assets/js/app.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/dist/simplebar.js') }}"></script>
     @stack('script')
 </body>
 
